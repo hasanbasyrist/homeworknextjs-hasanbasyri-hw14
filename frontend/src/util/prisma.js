1f34ae5cbd4662cf62bdeb43eb5dcf64964aaf7d
hasanbasyrist <123726617+hasanbasyrist@users.noreply.github.com>
2023-04-11 02:00:03 +0800

    Delete frontend/src/util directory

--- frontend/src/util/prisma.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma =
-    global.prisma || new PrismaClient({
-        Log: ["query"],
-    });
-        if(process.env.NODE_ENV != 'production') global.prisma
-export { prisma }; 
