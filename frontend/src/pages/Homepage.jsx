2683f1fa02eeba0368c93c84b9fb254b836fc001
hasanbasyrist <123726617+hasanbasyrist@users.noreply.github.com>
2023-04-11 01:14:39 +0800

    finish

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,45 @@
+import { VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Books from "../components/Books";
+import { getAllBooks } from "../modules/fetch";
+import { prisma } from "../util/prisma";
+
+// server side props ( fetch in server)
+
+export async function getServerSideProps() {
+  try {
+    const books = await prisma.book.findMany({
+     orderBy: {
+    title: "asc",
+     },
+    });
+  return {
+    props: {
+      books:booksJson.books
+    },
+  }
+} catch (err){
+  console.log(err);
+  return resizeBy.status(400).json({message: "something went wrong"});
+}
+}
+
+
+export default function Homepage() {
+  const [books, setBooks] = useState([]);
+  // useEffect(() => {
+  //   const fetchBooks = async () => {
+  //     const books = await getAllBooks();
+  //     setBooks(books);
+  //   };
+  //   fetchBooks();
+  // }, []);
+
+  return (
+    <VStack w="100vw">
+      {books?.books?.map((book) => (
+        <Books key={`${book.id} ${book.title}`} {...book} />
+      ))}
+    </VStack>
+  );
+}
